653cb46c3380d2af50e536e47f030200801f2205
Ashhad Matlub <ashhadmatlub@gmail.com>
2025-10-22 00:48:10 +0500

    C.java

--- Week 16/question7/C.java
+++ Week 16/question7/C.java
@@ -5,4 +5,8 @@ public interface C {
     public int m();
 
     public abstract int n();
+
+    // this is an interface / blueprint which is kind of contract which tells what must exists in those classes 
+    // which implements these methods 
+
 }
