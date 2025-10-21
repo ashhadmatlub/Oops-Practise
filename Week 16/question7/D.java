fa8bdf5abb51e9eec63721c46127bc015337ad54
Ashhad Matlub <ashhadmatlub@gmail.com>
2025-10-22 00:50:12 +0500

    D.java

--- Week 16/question7/D.java
+++ Week 16/question7/D.java
@@ -5,4 +5,8 @@ abstract class D implements C {
     public int m() {
         return 3;
     }
+
+    // an abtract class tells some ideas how to it 
+    // this class do not have the nessecity that it must override those methods of it's parent implemented class
 }
+
