f245cc6969e6573ddac67aadf2bf69e7fd80428d
Ashhad Matlub <ashhadmatlub@gmail.com>
2025-10-22 00:53:53 +0500

    F.java

--- Week 16/question7/F.java
+++ Week 16/question7/F.java
@@ -4,5 +4,8 @@ public class F extends E {
     // inherits m , n , o
     // this is a concrete class which complete all the things and inherits things from it ancestors classes 
     // or this class is a final class .
+    // abstract and interfaces lies in the method area they do not have the power to allocate spase in memory 
+    // whereas the subclass( anonymous class )  of an abstract class have this power to allocate spase in memory.
 }
+
 
