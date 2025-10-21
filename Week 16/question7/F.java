d6da3748287a4d7e9f2328d7e3cabeaf3295ec85
Ashhad Matlub <ashhadmatlub@gmail.com>
2025-10-22 00:51:47 +0500

    F.java

--- Week 16/question7/F.java
+++ Week 16/question7/F.java
@@ -2,4 +2,7 @@ package question7;
 
 public class F extends E {
     // inherits m , n , o
+    // this is a concrete class which complete all the things and inherits things from it ancestors classes 
+    // or this class is a final class .
 }
+
